f6eb635bbe61c34114b12f992050636d313aece6
Alexus Wong <alexus888@gmail.com>
2025-06-05 18:27:31 -0500

    Chapter 5
     - implement a new directory structure for the project
     - modify modules to respect new dir structure
     - don't fight travis and requirements
     - Dockerfile respects new entry point
     - add all the empty init modules
     - add a helper method for adding batches via REST API
     - use the REST API for adding batches in conftest instead of a helper fixture
     - add service to add a batch from primitives
     - modify allocate service to use primitives as well

--- adapters/orm.py
+++ adapters/orm.py
@@ -1,8 +1,7 @@
 from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import registry as Registry
-import model
-
+from domain import model
 
 registry = Registry()
 metadata = MetaData()
